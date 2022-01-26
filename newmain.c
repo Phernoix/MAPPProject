2aa8e96cbf0df8a5860f9c7f11f2a906630392c6
Phernoix <eklaseboer@gmail.com>
2022-01-26 19:13:41 +0800

    added comeback function which is the function used for determining whether the motor moves in

--- newmain.c
+++ newmain.c
@@ -6,16 +6,20 @@ unsigned char j;
 void main(void) {
     TRISB = 0b11111111; //RB5 to RB3 are connected to On/Off switches
     TRISD = 0b00000000; //RD7 to RD0 are connected to LEDs
-    ldrTest();
+    comeback();
+    
     return;
     
     
-    
 }
+//This function turns off LEDs at PORTD if the LDR detects light (use phone flashlight)
+//When LDR reads 0, it means it is bright (there is light)
+//When LDR reads 1, it means it is dim
 
-int ldrTest(){              //This function turns off LEDs at PORTD if the LDR detects light (use phone flashlight)
+
+int ldrTest(){              
     while (1){
-        if (PORTBbits.RB3 == 1)  {
+        if (PORTBbits.RB3 == 1)  { // 1
             PORTD = 0xFF; //light up all the LEDs on the PIC18 board
             delay_ms(1000);
         } else {
@@ -55,5 +59,23 @@ int motorTest(){
         delay_ms(1000);
     }
     return 0;
+}
+
+int comeback(){
+    while (1){
+        if (PORTBbits.RB3== 0 && PORTBbits.RB5==1){
+            PORTD= 0b00101011; // turns both motors
+            delay_ms(5000);
+            PORTDbits.RD5 = 0; PORTDbits.RD4 = 1; // reverses Motor 2
+            delay_ms(5000);
+            PORTDbits.RD0 = 0; // stops Motor1
+            delay_ms(5000);
+            PORTDbits.RD3 = 0; // stops Motor2
+            delay_ms(5000);
+            
+        } else{
+            return;
+        } 
+    }
 }
 
