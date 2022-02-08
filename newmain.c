9553992d9fa305ab0a6df5efa46ae6c6dfee93c6
liang799 <neohtp@tutanota.com>
2022-02-08 22:07:29 +0800

    minor syntax changes

--- newmain.c
+++ newmain.c
@@ -24,9 +24,9 @@ void main(void) {
  * When LDR reads 0, it means it is bright (there is light)
  * When LDR reads 1, it means it is dim
  */
-int isDark(){              
-        if (LDR == 1)
-            return 1;
+int isDark() {              
+    if (LDR == 1)
+        return 1;
     return 0;
 }
 
@@ -36,22 +36,22 @@ int isDark(){
     * PORTBbits.RB5 
  */
 //This code lights up all LEDs at PORTD if the moisture sensor reads 1 (wet)
-int isWet(){         
-        if(MOIST_SENS == 1)
-            return 1;
+int isWet() {         
+    if(MOIST_SENS == 1)
+        return 1;
     return 0;
 }
 
-int comeback(){
+int comeback() {
     while (1){
-            PORTD= 0b00101011; // turns both motors
-            delay_ms(5000);
-            PORTDbits.RD5 = 0; PORTDbits.RD4 = 1; // reverses Motor 2
-            delay_ms(5000);
-            PORTDbits.RD0 = 0; // stops Motor1
-            delay_ms(5000);
-            PORTDbits.RD3 = 0; // stops Motor2
-            delay_ms(5000);
+        PORTD= 0b00101011; // turns both motors
+        delay_ms(5000);
+        PORTDbits.RD5 = 0; PORTDbits.RD4 = 1; // reverses Motor 2
+        delay_ms(5000);
+        PORTDbits.RD0 = 0; // stops Motor1
+        delay_ms(5000);
+        PORTDbits.RD3 = 0; // stops Motor2
+        delay_ms(5000);
     }
 }
 
