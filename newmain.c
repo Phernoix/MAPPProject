cf383c75373ed8730e78011a0d7067044f559196
liang799 <neohtp@tutanota.com>
2022-02-08 21:33:15 +0800

    deleted motor testing code

--- newmain.c
+++ newmain.c
@@ -4,10 +4,10 @@
 unsigned char j;
 
 void main(void) {
-    TRISB = 0b11111111; //RB5 to RB3 are connected to On/Off switches
-    TRISD = 0b00000000; //RD7 to RD0 are connected to LEDs
-    INTCONbits.GIE = 1;
-    INTCONbits.INT0IE = 1;
+    TRISB = 0b11111111;     // RB5 to RB3 are connected to On/Off switches
+    TRISD = 0b00000000;     // RD7 to RD0 are connected to LEDs
+    INTCONbits.GIE = 1;     // Enable global interrupt
+    INTCONbits.INT0IE = 1;  // Enable int for RB0
     
     while(1) {
         if (isDark() && isWet())
@@ -18,38 +18,24 @@ void main(void) {
     
     
 }
-//This function turns off LEDs at PORTD if the LDR detects light (use phone flashlight)
-//When LDR reads 0, it means it is bright (there is light)
-//When LDR reads 1, it means it is dim
-
-
+/* This function turns off LEDs at PORTD if the LDR detects light (use phone flashlight)
+ * When LDR reads 0, it means it is bright (there is light)
+ * When LDR reads 1, it means it is dim
+ */
 int isDark(){              
         if (PORTBbits.RB3 == 1) // 1
             return 1;
     return 0;
 }
-//RB5 is for Moisture sensor
-    //When moisture sensor is dry, it reads 0
-    //When moisture sensor is wet, it reads 1
-    //PORTBbits.RB5
+
+/* RB5 is for Moisture sensor
+    * When moisture sensor is dry, it reads 0
+    * When moisture sensor is wet, it reads 1
+    * PORTBbits.RB5 
+ */
 int isWet(){         //This code lights up all LEDs at PORTD if the moisture sensor reads 1 (wet)
         if(PORTBbits.RB5 == 1)
             return 1;
-    return 0;
-}
-
-int motorTest(){
-    while (1){
- // the PORTD bits: --In3 In4 En2 In2 In1 En1
-        PORTD= 0b00101011; // turns both motors
-        delay_ms(1000);
-        PORTDbits.RD5 = 0; PORTDbits.RD4 = 1; // reverses Motor 2
-        delay_ms(1000);
-        PORTDbits.RD0 = 0; // stops Motor1
-        delay_ms(1000);
-        PORTDbits.RD3 = 0; // stops Motor2
-        delay_ms(1000);
-    }
     return 0;
 }
 
