b780135410ade58ab06dc2d9f47493f81f05824b
liang799 <neohtp@tutanota.com>
2022-02-11 10:04:22 +0800

    moveMotor() works

--- newmain.c
+++ newmain.c
@@ -4,23 +4,15 @@
 
 #define LDR PORTBbits.RB3
 #define MOIST_SENS PORTBbits.RB5
-#define MOTOR1_EN1 PORTDbits.RD0
-#define MOTOR1_IN1 PORTDbits.RD1
-#define MOTOR1_IN2 PORTDbits.RD2
-#define MOTOR2_EN2 PORTDbits.RD3
-#define MOTOR2_IN4 PORTDbits.RD4
-#define MOTOR2_IN3 PORTDbits.RD5
 
 int isDark(void);
 int isWet(void);
 void interrupt overrideButton_isr(void);
-void enableMotors(void);
-void disableMotors(void);
 void moveMotor(void);
 void moveMotor_Opposite(void);
-void manualBrake(void);
 
 int motorTime = 1000;
+int stopMotorTime = 2000;
 bool outside = false;
 
 void main(void) {
@@ -28,12 +20,8 @@ void main(void) {
     TRISD = 0b00000000;     // RD7 to RD0 are connected to LEDs
     INTCONbits.GIE = 1;     // Enable global interrupt
     INTCONbits.INT0IE = 1;  // Enable int for RB0
-
-    bool hasMoved = true;
-    enableMotors();
-    manualBrake();
     
-    while(1) {
+    /*while(1) {
         if (MOIST_SENS ==0 && LDR ==0)  {
             moveMotor();
             outside = true;
@@ -45,8 +33,9 @@ void main(void) {
         }else{
             manualBrake();
         }
-    }
-    
+    }*/
+       
+   moveMotor();
 }
  
 /* This function turns off LEDs at PORTD if the LDR detects light (use phone flashlight)
@@ -75,11 +64,11 @@ int isWet(void) {
 
 
 void interrupt overrideButton_isr(void) {
-    if (outside) {
+    //if (outside) {
         INTCONbits.INT0IF = 0;      //clear flag
         moveMotor_Opposite();
         outside = false;
-    }
+    //}
 }
 
 
@@ -87,41 +76,20 @@ void interrupt overrideButton_isr(void) {
  * We are using a H bridge for the motor
  * https://www.modularcircuits.com/blog/articles/h-bridge-secrets/h-bridges-the-basics
  */
-void enableMotors(void) {
-    MOTOR1_EN1 = 1;
-    MOTOR2_EN2 = 1;    
-}
-
-void disableMotors(void) {
-    MOTOR1_EN1 = 0;
-    MOTOR2_EN2 = 0;
-}
 
 void moveMotor(void) {
-    
-    MOTOR1_IN1 = 1;
-    MOTOR1_IN2 = 0;
-    MOTOR2_IN3 = 1;
-    MOTOR2_IN4 = 0;
+    PORTD = 0b00011000;
     delay_ms(motorTime);
+    PORTDbits.RD3 = 0; // stops Motor2
+    delay_ms(stopMotorTime);
+
 }
 
 void moveMotor_Opposite(void) {
-    
-    MOTOR1_IN1 = 0;
-    MOTOR1_IN2 = 1;
-    MOTOR2_IN3 = 0;
-    MOTOR2_IN4 = 1;
+    PORTD = 0b00101000;
     delay_ms(motorTime);
-}
-
-void manualBrake(void) {
-    
-    MOTOR1_IN1 = 0;
-    MOTOR1_IN2 = 0;
-    MOTOR2_IN3 = 1;
-    MOTOR2_IN4 = 1;
-    delay_ms(motorTime);
+    PORTD = 0; // stops Motor2
+    delay_ms(stopMotorTime);
 }
 
 
